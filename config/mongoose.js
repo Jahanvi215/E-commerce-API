25e21f4ef8db77d351411f6462f5ebfce59158a1
Jahanvi215 <tharu.jahanvi215@gmail.com>
2023-06-02 10:19:22 +0530

    file uploading

---
+++ config/mongoose.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+// connecting mongoose to its default server and ecommerceDB
+mongoose.connect('mongodb+srv://tharujahanvi215:Jahanvi123@cluster0.xqlfetj.mongodb.net/?retryWrites=true&w=majority', {
+    useNewUrlParser: true
+});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
+
+db.once('open', function(){
+    console.log('Connected to Database :: MongoDB');
+});
+
+module.exports = db;
+
+
